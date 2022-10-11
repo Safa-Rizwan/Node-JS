22fc86e0aae3a0428a8888440d93690cd042109d
Safa Rizwan <safa4302228@cloud.neduet.edu.pk>
2022-10-11 11:02:47 +0500

    Mongoose

--- mongoose/app.js
+++ mongoose/app.js
@@ -8,7 +8,7 @@ const sisters = new mongoose.Schema({
     active: Boolean
 })
 // Collection creation and insertion
-const intro = new mongoose.model("Sister",sisters);
+// const intro = new mongoose.model("Sister",sisters);
 const createDocument=async()=>{
     try{
 const sister1= new intro({
@@ -31,18 +31,27 @@ catch(error){
 // Read Document
 const getDocument=async()=>{
     try{
-    const result= await intro.find().select({age:{$in:["12","11"]}});
+    const result= await intro.find({age:{$in:["12","11"]}}).select({name:1});
     console.log(result)
 }catch(error){
     console.log(error)
 }}
 // getDocument();
 // Update Query
-const updateDocument= async()=>{
+const updateDocument= async(_id)=>{
     try{
-        const result=await sisters.updateOne(('63316aa8782edbc11a14ccb0'),{$set:{name:"Safa Rizwan"}})
+        const result=await intro.updateOne({_id},{$set:{name:"Safa Rizwan"}}).select({name:1})
     }catch(error){
         console.log(error)
     }
 }
-updateDocument();
+// updateDocument('63316aa8782edbc11a14ccb0');
+const deleteDocument = async (_id)=>{
+    try{
+        const result= await Sister.deleteOne({_id});
+        console.log(result)
+    }catch(error){
+        console.log(error)
+    }
+}
+deleteDocument('63316bc93ef5dc7754834aa5')
